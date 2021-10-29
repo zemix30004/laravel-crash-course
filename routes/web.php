761b6df858eb0d0723faade0239e55b1603ce37d
zemix30004 <eugenezm@gmail.com>
2021-10-29 13:49:05 +0300

    added bootstrap 5.1

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,3 @@ use \App\Http\Controllers\BlogController;
 Route::get('/', [BlogController::class, 'index']);
 Route::get('/category/{slug}', [BlogController::class, 'getPostsByCategory'])->name('getPostsByCategory');
 Route::get('/category/{slug_category}/{slug_post}', [BlogController::class, 'getPost'])->name('getPost');
-	
-   
