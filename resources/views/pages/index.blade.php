ffe9abed64e8b39c16d14e13fabdde27f4594ffe
zemix30004 <eugenezm@gmail.com>
2021-10-29 13:34:29 +0300

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main-layout')
+
+@section('title', 'My blog')
+
+@section('content')
+      
+    @foreach($posts as $post)
+    <div class="card mb-4">
+        <div class="card-header">
+            <a href="{{route('getPostsByCategory', $post->category['slug'])}}">{{$post->category['title']}}</a>
+        </div>
+        
+        <div class="card-body">
+            <h5 class="card-title">{{$post->title}}</h5>
+            <p class="card-text">{{$post->description}}</p>
+            <a href="{{route('getPost', [$post->category['slug'], $post->slug])}}" class="btn btn-primary">Read more</a>
+        </div>
+    </div>
+    @endforeach
+    {{$posts->links('vendor.pagination.bootstrap-4')}}
+   
+
+@endsection
